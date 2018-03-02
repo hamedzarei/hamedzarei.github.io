5ca319d0210603a12b1fd9a2501c20f848cc2123
Hamed Zarei <hamedzarei.7232@gmail.com>
2018-03-02 22:08:34 +0330

    first homepage

---
+++ back.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+    <title> welcome! </title>
+    <style>
+        body {
+            background: #e2e1e0;
+            text-align: center;
+        }
+
+        div#main {
+            padding: 20px;
+            width: 50%;
+            height: 500px;
+
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+
+            margin: auto;
+        }
+
+        #main div {
+            margin-top: 10px;
+        }
+
+        #main ul * {
+            text-align: left;
+
+        }
+
+        .card {
+            background: #fff;
+            border-radius: 2px;
+            display: inline-block;
+            margin: 1rem;
+            position: relative;
+        }
+        .card-1 {
+            box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+            transition: all 0.3s cubic-bezier(.25, .8, .25, 1);
+        }
+
+        .card-1:hover {
+            box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+        }
+    </style>
+</head>
+<body>
+<div id="main" class="card card-1">
+    <div>Hamed Zarei</div>
+    <div> What do you want?</div>
+    <div>
+        <ul>
+            <li> Building your own website?</li>
+            <li> Building desktop application?</li>
+            <li> Testing your website to identify security hole?</li>
+            <li> Do you want person     who can maintain every types of applications?</li>
+            <li> Want to learn programming?</li>
+        </ul>
+        <div> My Email: hamedzarei.7232@gmail.com</div>
+        <div> My Phone Number: +9809124174788</div>
+        <div>You can see my resume and experience in <a href="https://www.linkedin.com/in/hamedzarei/">my linkedin
+                page</a></div>
+
+    </div>
+
+</div>
+</body>
+</html>
